bc0ae78e7ab22596d5163c68d21805b24124eecb
Muhammet Akyol <muhammet@LWS-MC07s-MacBook-Pro.local>
2024-07-23 16:50:55 +0200

    feat:add calculation function

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const buttons = document.querySelectorAll(".button");
 
 let firstOp = "";
 let secondOp = "";
+let operator = "";
 
 const showDisplay = () => {
   if (!firstOp && !secondOp) {
@@ -15,9 +16,81 @@ const showDisplay = () => {
 };
 
 const buttonHandler = (event) => {
-  console.log(event.target.textContent);
+  const txt = event.target.textContent;
+  console.log(txt);
+
+  if (checkInput(txt)) {
+    if (!operator) {
+      if (txt === "." && firstOp.includes(".")) {
+        return;
+      }
+      firstOp = firstOp + txt;
+    } else {
+      if (txt === "." && secondOp.includes(".")) {
+        return;
+      }
+      secondOp = secondOp + txt;
+    }
+  } else {
+    if ("+-/X".includes(txt)) {
+      calculate();
+      operator = txt;
+    }
+    if (txt === "AC") {
+      firstOp = "";
+      secondOp = "";
+      operator = "";
+    }
+    if (txt === "=") {
+      calculate();
+      operator = "";
+    }
+    if (txt === "+/-") {
+      if (!operator) {
+        if (firstOp.startsWith("-")) {
+          firstOp = firstOp.substring(1);
+        } else {
+          firstOp = "-" + firstOp;
+        }
+      } else {
+        if (secondOp.startsWith("-")) {
+          secondOp = secondOp.substring(1);
+        } else {
+          secondOp = "-" + secondOp;
+        }
+      }
+    }
+  }
+  showDisplay();
+};
+
+const calculate = () => {
+  if (firstOp && secondOp && operator) {
+    const firstValue = parseFloat(firstOp);
+    const secondValue = parseFloat(secondOp);
+    if (operator === "+") {
+      firstOp = (firstValue + secondValue).toString();
+    }
+    if (operator === "-") {
+      firstOp = (firstValue - secondValue).toString();
+    }
+    if (operator === "X") {
+      firstOp = (firstValue * secondValue).toString();
+    }
+    if (operator === "/") {
+      if (secondValue === 0) {
+        firstOp = "fehler";
+      } else {
+        firstOp = (firstValue / secondValue).toString();
+      }
+    }
+    operator = "";
+    secondOp = "";
+  }
 };
 
 for (const button of buttons) {
   button.addEventListener("click", buttonHandler);
 }
+
+const checkInput = (input) => !isNaN(input) || input === ".";
